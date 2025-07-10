6a24ad8351de2a7cd278b31b3ffc26ca0887c2f0
samyak1001 <jainsamyak466@gmail.com>
2025-07-10 23:16:52 +0530

    feat:Add Java Project

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Multiply
+{
+    Public Static void main [String [] args]{
+        int a=5
+        for(int i=0;i=i*10;i++){
+            System.out.println(a*i);
+        }
+    }
+}        
